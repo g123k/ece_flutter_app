fd163d33671295346c0faa07a8878b90c25196db
Edouard Marquez <moi@edouard-marquez.me>
2025-03-21 10:38:51 +0100

    Ecran fiche

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ece_app/res/app_colors.dart';
 import 'package:ece_app/screens/details/product_details.dart';
 import 'package:flutter/material.dart';
 
@@ -15,8 +16,48 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Avenir',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        primaryColor: AppColors.blue,
+        primaryColorLight: AppColors.blueLight,
+        primaryColorDark: AppColors.blueDark,
+        appBarTheme: const AppBarTheme(
+          centerTitle: false,
+          backgroundColor: AppColors.white,
+          elevation: 0.0,
+          iconTheme: IconThemeData(color: AppColors.blue),
+          titleTextStyle: TextStyle(
+            color: AppColors.blue,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: AppColors.blue,
+            fontSize: 22.0,
+            fontWeight: FontWeight.bold,
+          ),
+          displayMedium: TextStyle(color: AppColors.gray2, fontSize: 18.0),
+          headlineMedium: TextStyle(color: AppColors.gray3, fontSize: 17.0),
+          headlineSmall: TextStyle(
+            color: AppColors.blue,
+            fontSize: 17.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        dividerColor: AppColors.gray2,
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedItemColor: AppColors.blue,
+          unselectedItemColor: AppColors.gray2,
+          type: BottomNavigationBarType.fixed,
+        ),
+        navigationBarTheme: const NavigationBarThemeData(
+          indicatorColor: AppColors.blue,
+        ),
       ),
-      home: ProductDetailsPage(),
+      home: ProductDetails(),
     );
   }
 }
