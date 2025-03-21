b91477a52de67ee8e0471d3150bc19247610a2a1
Edouard Marquez <moi@edouard-marquez.me>
2025-03-21 12:28:03 +0100

    Onglets

--- lib/screens/details/product_details.dart
+++ lib/screens/details/product_details.dart
@@ -1,5 +1,8 @@
 import 'package:ece_app/res/app_icons.dart';
 import 'package:ece_app/screens/details/tabs/product_tab0.dart';
+import 'package:ece_app/screens/details/tabs/product_tab1.dart';
+import 'package:ece_app/screens/details/tabs/product_tab2.dart';
+import 'package:ece_app/screens/details/tabs/product_tab3.dart';
 import 'package:flutter/material.dart';
 
 class ProductDetails extends StatefulWidget {
@@ -15,7 +18,37 @@ class _ProductDetailsState extends State<ProductDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ProductPageTab0(),
+      body: SizedBox.expand(
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Offstage(
+                offstage: _currentTab != ProductDetailsCurrentTab.summary,
+                child: ProductPageTab0(),
+              ),
+            ),
+            Positioned.fill(
+              child: Offstage(
+                offstage: _currentTab != ProductDetailsCurrentTab.info,
+                child: ProductPageTab1(),
+              ),
+            ),
+            Positioned.fill(
+              child: Offstage(
+                offstage: _currentTab != ProductDetailsCurrentTab.nutrition,
+                child: ProductPageTab2(),
+              ),
+            ),
+            Positioned.fill(
+              child: Offstage(
+                offstage:
+                    _currentTab != ProductDetailsCurrentTab.nutritionalValues,
+                child: ProductPageTab3(),
+              ),
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentTab.index,
         onTap: (int position) {
